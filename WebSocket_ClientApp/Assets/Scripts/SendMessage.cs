ce159b42b901e4d24fbfb43d68a1001817586421
takaishi keito <keitoncis@icloud.com>
2019-01-31 13:08:49 +0900

    sample app

---
+++ WebSocket_ClientApp/Assets/Scripts/SendMessage.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+
+public class SendMessage : MonoBehaviour {
+	WebSocket ws;
+	void Start () {
+		//portとipの指定 
+		ws = new WebSocket("ws://localhost:3000/");
+		
+		//イベント処理の追加（文法はラムダ式とかってググるとわかりやすいかも）
+		ws.OnOpen += (sender, e) =>
+		{
+			Debug.Log("WebSocket Open");
+		};
+
+		ws.OnMessage += (sender, e) =>
+		{
+			Debug.Log("Data: " + e.Data);
+		};
+
+		ws.OnError += (sender, e) =>
+		{
+			Debug.Log("WebSocket Error Message: " + e.Message);
+		};
+
+		ws.OnClose += (sender, e) =>
+		{
+			Debug.Log("WebSocket Close");
+		};
+
+		ws.Connect();
+
+	}
+	
+	//データの送信
+	void Update () {
+		if (Input.GetKeyUp("s"))
+		{
+			ws.Send("Test Message");
+		}
+	}
+	//接続を断つ
+	void OnDestroy()
+	{
+		ws.Close();
+		ws = null;
+	}
+}
---
+++ WebSocket_ServerApp/Assets/Scripts/ServerExample.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+
+public class ServerExample: MonoBehaviour {
+
+    WebSocketServer server;
+
+    private void OnEnable()
+    {
+        //serverオブジェクトの生成
+        server = new WebSocketServer(3000);
+    }
+
+    void Start ()
+    {
+        server.AddWebSocketService<TestFunc>("/");
+        server.Start();
+
+    }
+
+    void OnDestroy()
+    {
+        server.Stop();
+        server = null;
+    }
+
+}
+
+public class TestFunc : WebSocketBehavior
+{
+    protected override void OnOpen()
+    {
+        Debug.Log ("opend");
+    }
+    
+    protected override void OnMessage (MessageEventArgs e)
+    {
+        Debug.Log("Comming Message ! : " + e.Data);
+    }
+}
